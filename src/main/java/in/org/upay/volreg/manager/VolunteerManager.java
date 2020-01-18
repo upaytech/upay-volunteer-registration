111db968b5c5629c9986548dbd750b0e238cb187
Piyush Ranjan <piyushranjan95@gmail.com>
2020-01-18 23:25:02 +0530

    Fix props and use them

--- src/main/java/in/org/upay/volreg/manager/VolunteerManager.java
+++ src/main/java/in/org/upay/volreg/manager/VolunteerManager.java
@@ -1,12 +1,16 @@
 package in.org.upay.volreg.manager;
 
+import in.org.upay.volreg.VolregApplication;
 import in.org.upay.volreg.dto.VolunteerRegistration;
 import in.org.upay.volreg.email.EmailService;
 import in.org.upay.volreg.email.EmailTemplate;
 import in.org.upay.volreg.model.Volunteer;
 import in.org.upay.volreg.repository.VolunteerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -17,6 +21,20 @@ import java.util.Arrays;
 @Component
 public class VolunteerManager {
 
+    @Value("${contact.email.pune}")
+    private String EMAIL_PUNE;
+    @Value("${contact.email.mouda}")
+    private String EMAIL_MOUDA;
+    @Value("${contact.email.delhi}")
+    private String EMAIL_DELHI;
+    @Value("${contact.email.bangalore}")
+    private String EMAIL_BANGALORE;
+    @Value("${contact.email.ncr}")
+    private String EMAIL_NCR;
+    @Value("${contact.email.default}")
+    private String EMAIL_DEFAULT;
+    @Value("${contact.email.nagpur}")
+    private String EMAIL_NAGPUR;
 
     @Autowired
     private EmailService emailService;
@@ -24,25 +42,27 @@ public class VolunteerManager {
     @Autowired
     private VolunteerRepository repository;
 
+    private static final Logger log = LoggerFactory.getLogger(VolregApplication.class);
+
     private String[] getNotificationAddresses(String cityName) {
         // todo: read emails from env based properties file
         String emails = "";
         switch (cityName.toUpperCase()) {
-            case "PUNE": emails = "upaypune@gmail.com,upaypr@gmail.com";
+            case "PUNE": emails = EMAIL_PUNE;
             break;
-            case "MOUDA": emails = "upaymda@gmail.com,upaypr@gmail.com";
+            case "MOUDA": emails = EMAIL_MOUDA;
             break;
-            case "NAGPUR": emails = "upayngp@gmail.com,upaypr@gmail.com";
+            case "NAGPUR": emails = EMAIL_NAGPUR;
             break;
             case "NOIDA":
-            case "NEW DELHI": emails = "upaydelhi@gmail.com,upaypr@gmail.com";
+            case "NEW DELHI": emails = EMAIL_DELHI;
             break;
-            case "GURGAON": emails = "upayncr@gmail.com,upaypr@gmail.com";
+            case "GURGAON": emails = EMAIL_NCR;
             break;
-            case "BANGALORE": emails = "upaybangalore@gmail.com,upaypr@gmail.com";
+            case "BANGALORE": emails = EMAIL_BANGALORE;
             break;
             default:
-                emails = "upaypr@gmail.com";
+                emails = EMAIL_DEFAULT;
         }
 
         return emails.split(",");
