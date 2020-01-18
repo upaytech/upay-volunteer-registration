91af062e6230615d4baafa78ebed9abe33d9d758
Piyush Ranjan <piyushranjan95@gmail.com>
2020-01-18 23:42:27 +0530

    Add extra academy field in request

--- src/main/java/in/org/upay/volreg/manager/VolunteerManager.java
+++ src/main/java/in/org/upay/volreg/manager/VolunteerManager.java
@@ -73,7 +73,7 @@ public class VolunteerManager {
         String subject = String.format(EmailTemplate.REGISTRATION_EMAIL_MESSAGE, registration.getCityName());
         String text = String.format(EmailTemplate.HTML_EMAIL_CONTENT, registration.getName(), registration.getEmail(), registration.getMobile(),
                                     registration.getQualification(), registration.getCityName(), Arrays.toString(registration.getContributionMethod())
-                                   , registration.getWhyUpay(), registration.getExperienced());
+                                   , registration.getWhyUpay(), registration.getExperienced(), registration.getExtraAcademy());
         emailService.sendEmail(to, subject, text);
     }
 
